b6652679e09aeef4531d413d73c97f81d490538f
Aditya Surana <to.adityasurana@gmail.com>
2020-05-12 19:27:34 +0530

    Add files via upload

---
+++ uploads/core/templates/core/twitter_tool.js
@@ -0,0 +1,17 @@
+function hideme() {
+	$('#loader').hide();
+}
+hideme();
+
+$("#hide").click(function() {
+	$("#loader").show();
+};
+
+function hideme1() {
+	$('#loader1').hide();
+}
+hideme1();
+
+$("#hide1").click(function() {
+	$("#loader1").show();
+};
